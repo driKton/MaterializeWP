190692deb838b4b17a548398b7ac58fe89867a5f
Carlos Daniel Moreno Rodríguez <Carlos@iMac-de-Carlos.local>
2016-12-09 18:50:49 +0000

    Added theme functions file

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Funciones de MaterializeWP
+ *
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @link https://codex.wordpress.org/Child_Themes
+ *
+ *
+ * @package WordPress
+ * @subpackage MaterializeWP
+ */
+
+/**
+ * MaterializeBoss únicamente funciona en WordPress >4.7
+ */
